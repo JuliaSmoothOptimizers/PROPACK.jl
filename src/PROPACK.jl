313f71a60f5a99bed34ba67bd07e7792bea9219e
Jiahao Chen <jiahao@mit.edu>
2016-03-24 23:10:00 -0600

    Fix deprecation warnings
    tsvdvals now returns error bars
    
    Preliminary _irl wrappers

--- src/PROPACK.jl
+++ src/PROPACK.jl
@@ -1,6 +1,6 @@
 module PROPACK
 
-export tsvd, tsvdvals
+export tsvd, tsvdvals, tsvd_irl, tsvdvals_irl
 
 include("wrappers.jl")
 
@@ -11,7 +11,7 @@ global mvp1s = 0
 global mvp2s = 0
 
 # function __f__(transa, mm, nn, x, y, dparm, iparm)
-#     if char(unsafe_load(transa)) == 'n'
+#     if Char(unsafe_load(transa)) == 'n'
 #         A_mul_B!(1.0, __mat__, pointer_to_array(x, unsafe_load(nn)), 0.0, pointer_to_array(y, unsafe_load(mm)))
 #     else
 #         Ac_mul_B!(1.0, __mat__, pointer_to_array(x, unsafe_load(mm)), 0.0, pointer_to_array(y, unsafe_load(nn)))
@@ -19,7 +19,9 @@ global mvp2s = 0
 #     nothing
 # end
 function __f__(transa, mm, nn, x, y, dparm, iparm)
-    if char(unsafe_load(transa)) == 'n'
+    global mvp1s
+    global mvp2s
+    if Char(unsafe_load(transa)) == 'n'
         tmp = __mat__*pointer_to_array(x, unsafe_load(nn))
         unsafe_copy!(y, pointer(tmp), unsafe_load(mm))
         mvp1s += 1
@@ -31,7 +33,7 @@ function __f__(transa, mm, nn, x, y, dparm, iparm)
     nothing
 end
 
-function tsvd{T}(A; initvec::Vector{T} = zeros(T, size(A, 1)), k::Integer = 1, kmax::Integer = 1000, tolin::Real = sqrt(eps(real(one(T)))))
+function tsvd{T}(A::AbstractMatrix{T}; initvec::Vector{T} = zeros(T, size(A, 1)), k::Integer = 1, kmax::Integer = 1000, tolin::Real = sqrt(eps(real(one(T)))))
 
     m, n = size(A)
     global __mat__ = A
@@ -41,7 +43,7 @@ function tsvd{T}(A; initvec::Vector{T} = zeros(T, size(A, 1)), k::Integer = 1, k
 
     lansvd('Y', 'Y', m, n, __pf__, initvec, k, kmax, tolin)
 end
-function tsvdvals{T}(A; initvec::Vector{T} = zeros(T, size(A, 1)), k::Integer = 1, kmax::Integer = 1000, tolin::Real = sqrt(eps(real(one(T)))))
+function tsvdvals{T}(A::AbstractMatrix{T}; initvec::Vector{T} = zeros(T, size(A, 1)), k::Integer = 1, kmax::Integer = 1000, tolin::Real = sqrt(eps(real(one(T)))))
 
     m, n = size(A)
     global __mat__ = A
@@ -49,7 +51,7 @@ function tsvdvals{T}(A; initvec::Vector{T} = zeros(T, size(A, 1)), k::Integer = 
     global mvp2s = 0
     __pf__ = cfunction(__f__, Void, (Ptr{UInt8}, Ptr{Int32}, Ptr{Int32}, Ptr{T}, Ptr{T}, Ptr{T}, Ptr{Int32}))
 
-    lansvd('N', 'N', m, n, __pf__, initvec, k, kmax, tolin)[2]
+    lansvd('N', 'N', m, n, __pf__, initvec, k, kmax, tolin)[[2,4]]
 end
 
 end # module
--- src/wrappers.jl
+++ src/wrappers.jl
@@ -56,11 +56,11 @@ for (fname, lname, elty) in ((:slansvd_, :libspropack, Float32),
 
             nb = 10 # BLAS-3 blocking size. Don't know the size
             if jobu == 'N' && jobv == 'N'
-                lwork = int32(m + n + 9kmax + 2kmax*kmax + 4 + max(m + n, 4kmax + 4))
-                liwork = int32(2kmax + 1)
+                lwork = Int32(m + n + 9kmax + 2kmax*kmax + 4 + max(m + n, 4kmax + 4))
+                liwork = Int32(2kmax + 1)
             else
-                lwork = int32(m + n + 9kmax + 5kmax*kmax + 4 + max(3kmax*kmax + 4kmax + 4, nb*max(m, n)))
-                liwork = int32(8kmax)
+                lwork = Int32(m + n + 9kmax + 5kmax*kmax + 4 + max(3kmax*kmax + 4kmax + 4, nb*max(m, n)))
+                liwork = Int32(8kmax)
             end
             work = Array($elty, lwork)
             iwork = Array(Int32, liwork)
@@ -74,6 +74,86 @@ for (fname, lname, elty) in ((:slansvd_, :libspropack, Float32),
             lansvd!(jobu, jobv, m, n, kmax, pff, U, s, bnd, V, tolin, work,
                 iwork, doption, ioption, dparm, iparm)
 
+        end
+    end
+end
+
+
+for (fname, lname, elty) in ((:slansvd_irl_, :libspropack, Float32),
+                             (:dlansvd_irl_, :libdpropack, Float64))
+    @eval begin
+        function lansvd_irl!(which::Char, jobu::Char, jobv::Char, m::Integer,
+            n::Integer, dim::Integer, p::Integer, neig::Integer,
+            maxiter::Integer, aprod, U::StridedMatrix{$elty}, s::Vector{$elty},
+            bnd::Vector{$elty}, V::StridedMatrix{$elty}, tolin::$elty,
+            work::Vector{$elty}, iwork::Vector{Int32}, doption::Vector{$elty},
+            ioption::Vector{Int32}, dparm::Vector{$elty}, iparm::Vector{Int32})
+
+            # extract values
+            k = length(s)
+            ldu = stride(U, 2)
+            ldv = stride(V, 2)
+
+            # check
+
+            # allocate
+            info = Int32[0]
+
+            ccall(($(string(fname)), $lname), Void,
+                (Ptr{UInt8}, Ptr{UInt8}, Ptr{UInt8}, Ptr{Int32}, Ptr{Int32},
+                 Ptr{Int32}, Ptr{Int32}, Ptr{Int32}, Ptr{Void}, Ptr{$elty},
+                 Ptr{Int32}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+                 Ptr{Int32}, Ptr{$elty}, Ptr{$elty}, Ptr{Int32},
+                 Ptr{Int32}, Ptr{Int32}, Ptr{$elty}, Ptr{Int32},
+                 Ptr{Int32}, Ptr{$elty}, Ptr{Int32}),
+                &which, &jobu, &jobv, &m, &n,
+                &dim, &p, &neig, &maxiter, aprod, U,
+                &ldu, s, bnd, V,
+                &ldv, &tolin, work, &length(work),
+                iwork, &length(iwork), doption, ioption,
+                info, dparm, iparm)
+
+
+
+            if info[1] != 0
+                error("info was $(info[1])")
+            end
+
+            return U, s, V, bnd
+        end
+
+        function lansvd_irl!(which::Char, jobu::Char, jobv::Char, m::Integer,
+            n::Integer, dim::Integer, p::Integer, neig::Integer,
+            pff::Ptr{Void}, initvec::Vector{$elty}, k::Integer, kmax::Integer,
+            tolin::$elty)
+
+            # Extract
+
+            U = Array($elty, m, kmax + 1)
+            copy!(U, 1, initvec, 1, m)
+            s = Array($elty, k)
+            bnd = Array($elty, k)
+            V = Array($elty, m, kmax)
+
+            nb = 10 # BLAS-3 blocking size. Don't know the size
+            if jobu == 'N' && jobv == 'N'
+                lwork = Int32(m + n + 10kmax + 2kmax*kmax + 5 + max(m, max(n, 4kmax + 4)))
+                liwork = Int32(2kmax + 1)
+            else
+                lwork = Int32(m + n + 10kmax + 5kmax*kmax + 4 + max(3kmax*kmax + 4kmax + 4, nb*max(m, n)))
+                liwork = Int32(8kmax)
+            end
+            work = Array($elty, lwork)
+            iwork = Array(Int32, liwork)
+
+            doption = $elty[sqrt(eps($elty)); eps($elty)^(3/4); 0.0; zeros(7)]
+            ioption = Int32[0; 1; zeros(8)]
+
+            dparm = $elty[0]
+            iparm = Int32[0]
+
+            lansvd!(which, jobu, jobv, m, n, dim, p, neig, maxiter, pff, U, s,
+                bnd, V, tolin, work, iwork, doption, ioption, dparm, iparm)
         end
     end
 end
