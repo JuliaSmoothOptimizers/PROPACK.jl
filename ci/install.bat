b3d2ed5d05ad4f39ebe37811f03a5133f20390fd
Dominique Orban <dominique.orban@gmail.com>
2016-09-21 16:14:38 -0400

    windows: link against openblas

--- ci/install.bat
+++ ci/install.bat
@@ -13,3 +13,4 @@ echo Bits: %BIT%
 SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 bash -lc "pacman --noconfirm --sync pacman-mirrors"
 bash -lc "pacman -S --needed --noconfirm git make mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool"
+bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-openblas"
