5119c75ff6566f8d6ccca6efa0c332e354dfa626
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-01-13 05:51:49 -0600

    Maintenance of PROPACK.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,14 @@ using Documenter, PROPACK
 makedocs(
   modules = [PROPACK],
   doctest = true,
-  # linkcheck = true,
-  strict = true,
-  format = Documenter.HTML(
-    assets = ["assets/style.css"],
-    prettyurls = get(ENV, "CI", nothing) == "true",
-  ),
+  linkcheck = true,
+  format = Documenter.HTML(assets = ["assets/style.css"],
+                           ansicolor = true,
+                           prettyurls = get(ENV, "CI", nothing) == "true"),
   sitename = "PROPACK.jl",
-  pages = Any["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
+  pages = ["Home" => "index.md",
+           "Tutorial" => "tutorial.md",
+           "Reference" => "reference.md"],
 )
 
 deploydocs(
